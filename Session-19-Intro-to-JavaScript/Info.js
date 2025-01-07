06b43a867cf2b12be70693459ac103a413b8f927
GeeksforgeeksDS <courses@geeksforgeeks.org>
2025-01-07 12:32:15 +0530

    introduction to javascript

---
+++ Session-19-Intro-to-JavaScript/Info.js
@@ -0,0 +1,47 @@
+// what is javascript?
+// JavaScript is a programming language that enables you to create dynamically updating content, control multimedia, animate images, and much more.
+// JavaScript is a high-level, interpreted programming language.
+// It is a programming language that is characterized as dynamic, weakly typed, prototype-based and multi-paradigm.
+// js is single threaded language
+// js is case sensitive language
+
+
+// what is javascript used for?
+// JavaScript is a text-based programming language used both on the client-side and server-side that allows you to make web pages interactive.
+// JavaScript is used to create interactive websites, web applications, games, and more.
+// JavaScript is used to add interactivity and dynamic content to web pages.
+
+
+// what is single thread language?
+// Single-threaded programming is the most basic form of programming. It means that the program can only execute one task at a time.
+// JavaScript is a single-threaded language, which means it has one call stack and one memory heap.
+
+
+// what is client side scripting
+// Client-side scripting refers to scripts that run within your web browser. JavaScript is the most common language used for client-side scripting.
+// JavaScript is a client-side scripting language, which means the source code is processed by the client's web browser rather than on the web server.
+// This means JavaScript functions can run after a webpage has loaded without communicating with the server.
+
+
+// what is server side scripting
+// Server-side scripting is a technique used in web development which involves employing scripts on a web server which produce a response customized for each user's (client's) request to the website.
+// Server-side scripting is often used to provide a customized interface for the user. The script can vary depending on the data provided by the user or the user's browser.
+
+
+// how to install node js in system
+// 1. Go to the official website of Node.js and download the installer.
+// 2. Run the installer (the .msi file you downloaded in the previous step.)
+// 3. Follow the prompts in the installer (Accept the license agreement, click the NEXT button a few times and accept the default installation settings).
+// 4. Restart your computer. You won’t be able to run Node.js until you restart your computer.
+
+
+// why js is dynamically typed language?
+// JavaScript is a dynamically typed language, which means that you don't have to specify the data type of a variable when you declare it.
+// JavaScript automatically determines the data type of the variable based on the value assigned to it.
+
+
+// why js is weakly typed language?
+// JavaScript is a weakly typed language, which means that it does not enforce strict data types.
+// This allows you to perform operations on variables without having to worry about their data types.
+// For example, you can add a number to a string in JavaScript without any errors.
+
