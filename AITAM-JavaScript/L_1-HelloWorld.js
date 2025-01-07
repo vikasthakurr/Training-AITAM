f3c14c25bc6235c430bc91baca0e437f2047f560
GeeksforgeeksDS <courses@geeksforgeeks.org>
2025-01-07 22:33:37 +0530

    javascript

---
+++ AITAM-JavaScript/L_1-HelloWorld.js
@@ -0,0 +1,12 @@
+// first program in Javascript
+
+/*console.log("hello AITAM");
+console.log("this batch is very smart");
+console.log("I am enjoying teaching them");
+*/
+
+// Different ways to print in Javascript
+console.log("Hello AITAM");
+window.alert("Hello AITAM"); // alert box will be displayed in the browser window
+document.write("Hello AITAM"); // it will write the content in the document
+
--- Session-19-Intro-to-JavaScript/helloworld.js
+++
@@ -1,3 +0,0 @@
-console.log("hello AITAM");
-console.log("this batch is very smart");
-console.log("I am enjoying teaching them");
